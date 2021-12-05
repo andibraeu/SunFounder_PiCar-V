a7bf5110590f4841627a269e05b9235debf4c336
Andreas Bräu <ab@andi95.de>
2021-12-05 13:24:34 +0100

    send a message to the chat
    Signed-off-by: Andreas Bräu <ab@andi95.de>

--- remote_control/static/jitsi/robot_control.js
+++ remote_control/static/jitsi/robot_control.js
@@ -50,6 +50,12 @@ api.on('incomingMessage', (message) => {
 
 window.setInterval(function () { count_drive_votes_and_drive() }, 1500);
 window.setInterval(function () { count_cam_votes_and_cam() }, 1500);
+window.setInterval(() => {
+    api.executeCommand('sendChatMessage', {
+    message: 'Hallo, ich bin Robby-Car. Wie ihr mich bedienen könnt findet ihr auf https://world.naturkunde.museum/tafeln/robby',
+    ignorePrivacy: false // true if the privacy notification should be ignored. Defaulted to false.
+    });
+}, 30000) 
 
 function enableOrDisableRobot(message) {
     if (enabled && message === 'disable') {
